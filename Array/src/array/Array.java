0d217e49a73ced7e7878b4e094e27959f15a366c
Sammy Alsadek <sammyalsadek@Sammys-Air.attlocal.net>
2020-03-28 19:24:13 -0700

    added an add method and constructors to the Array class

--- Array/src/array/Array.java
+++ Array/src/array/Array.java
@@ -1,21 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package array;
 
-/**
- *
- * @author sammyalsadek
- */
-public class Array {
+public class Array<T extends Object> 
+{
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private Object[] array;  // array of any data type
+    private int size;   // number of items in array
+    
+    // general constructor in case nothing was passed in 
+    public Array()
+    {
+        array = new Object[5];  // if no length is given create a 5 length array
+        size = 0;   // initialize the size at 0
+    }   
+    
+    // constructor for if the user inputs the arrays starting length
+    public Array(int length)
+    {
+        array = new Object[length]; // create an array with the length given
+        size = 0;   // size remains 0 until items are added
+    }    
+    
+    // method to add item into array
+    public void add(T item)
+    {
+        // if the array has to more space create a new array
+        if ( array.length <= size) 
+        {
+            // new array with double the space
+            Object[] new_array = new Object[array.length * 2]; 
+            
+            // copy old array to new array
+            for (int i = 0; i < size; i++)
+            {
+                new_array[i] = array[i];
+            }
+            
+            // set new array to array
+            array = new_array;
+            
+            //add item
+            add(item);
+        }
+        
+        // allocate item into the array and increment the size
+        array[size++] = item;
     }
     
 }
---
+++ Array/src/array/Main.java
@@ -0,0 +1,15 @@
+package array;
+
+public class Main 
+{
+    
+    public static void main(String[] args)
+    {
+        Array<Integer> array = new Array();
+        
+        array.add(1);
+        array.add(2);
+        
+    }
+    
+}
