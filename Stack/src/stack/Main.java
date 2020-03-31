00bebcd8dafbba2f1b45d055803ca3c0320273bf
Sammy Alsadek <sammyalsadek@Sammys-Air.attlocal.net>
2020-03-30 23:15:36 -0700

    completed my stack class

--- Stack/src/stack/Main.java
+++ Stack/src/stack/Main.java
@@ -1,9 +1,40 @@
 package stack;
 
-public class Main {
+public class Main 
+{
 
-   public static void main(String[] args) {
-
+   public static void main(String[] args) 
+   {
+       Stack<Integer> stack = new Stack<>();
+       
+       stack.push(1);
+       stack.push(2);
+       stack.push(3);
+       stack.push(4);
+       stack.push(5);
+       
+       stack.pop();
+       stack.pop();
+       stack.pop();
+       
+       stack.push(1);
+       stack.push(2);
+       
+       stack.pop();
+       stack.pop();
+       stack.pop();
+       stack.pop();
+       
+       stack.peek();
+       System.out.println("Empty = " + stack.empty());
+       
+       stack.push(1);
+       stack.push(2);
+       
+       stack.peek();
+       System.out.println("Empty = " + stack.empty());
+       
+       stack.search(2);
+       stack.search(5);
    }
-    
 }
