7160d5080f2d296e683982932fe6e767d89a234e
Sammy Alsadek <sammyalsadek@Sammys-Air.lan>
2020-12-17 15:12:50 -0800

    finalized fenwick tree

--- FenwickTree_(BinaryIndexedTree)/src/fenwicktree_/binaryindexedtree/FenwickTree.java
+++ FenwickTree_(BinaryIndexedTree)/src/fenwicktree_/binaryindexedtree/FenwickTree.java
@@ -1,90 +1,118 @@
 package fenwicktree_.binaryindexedtree;
 
+/**
+ * @author sammyalsadek
+ *
+ */
 public class FenwickTree {
-    
-    // this array contains the fenwick tree ranges
-    private long [] tree;
-    
-    //////////////////////////////////////////////////////////////////////////////
-   
-    // constructors
-    
-    // creates an empty fenwick tree
-    public FenwickTree(int size) {tree = new long [size + 1];}
-    
-    // make sure the 'values' array is one based meaning
-    // values[0] does not get used, O(n) construction
-    public FenwickTree(long [] values) {
-        
-        if (values == null) 
-            throw new IllegalArgumentException("Values array is null");
-        
-        // make a clone of the 'values' array since we manipulate
-        // the array in place destroying all its original content
-        tree = values.clone();
-        
-        for (int i = 1; i < tree.length; i++) {
-            int j = i + lsb(i);
-            if (j < tree.length) tree[j] += tree[i];
-        }
-        
-    }
-    
-    // returns the value of the least significant bit 
-    // lsb(5) => 5 => 0101 => 010(1) => 0001 => 1
-    private int lsb(int i) {
-        
-        // isolate the lowest one bit value
-        return i & -i;
-        
-        // example1: i = 5 = 0101
-        // -i = 1011 (2's compliment)
-        // 0101 & 1011 = 0001 = 1
-        
-        // example2: i = 8 = 1000
-        // -i = 1000 (2's compliment)
-        // 1000 & 1000 = 1000 = 8
-        
-        // another alternate method is to use java's built
-        // in method return Integer.lowestOneBit(i);
-        
-    }
-    
-    // computes the prefix sum from [1, i], one based
-    public long prefixSum(int i) {
-        long sum = 0L;
-        while (i != 0) {
-            sum += tree[i];
-            i &= ~lsb(i); // pretty much i -= lsb(i) 
-        }
-        return sum;
-    }
-    
-    // returns the sum of the interval [i, j], one based
-    public long sum(int i, int j) {
-        if (j < i) throw new IllegalArgumentException("j must be >= i");
-        return prefixSum(j) - prefixSum(i-1);
-    }
-    
-    // adds 'k' to index 'i', one based
-    public void add(int i, long k) {
-        while (i < tree.length) {
-            tree[i] += k;
-            i += lsb(i);
-        }
-    }
-    
-    // set index i to be equal to k, one based
-    public void set(int i, long k) {
-        long value = sum(i, i);
-        add(i, k - value);
-    }
-    
-    // returns a string of the fenwick tree
-    @Override public String toString() {
-        String string = "";
-        for (int i = 0; i < tree.length; i++) string += "[" + tree[i] + "]";
-        return string;
-    }
-    
+
+	// this array contains the fenwick tree ranges
+	private long[] tree;
+
+	//////////////////////////////////////////////////////////////////////////////
+
+	// constructors
+
+	// creates an empty fenwick tree
+	public FenwickTree(int size) {
+		tree = new long[size + 1];
+	}
+
+	// make sure the 'values' array is one based meaning
+	// values[0] does not get used, O(n) construction
+	public FenwickTree(long[] values) {
+
+		if (values == null)
+			throw new IllegalArgumentException("Values array is null");
+
+		// make a clone of the 'values' array since we manipulate
+		// the array in place destroying all its original content
+		tree = values.clone();
+
+		for (int i = 1; i < tree.length; i++) {
+
+			int j = i + lsb(i);
+
+			if (j < tree.length)
+				tree[j] += tree[i];
+
+		}
+
+	}
+
+	// returns the value of the least significant bit
+	// lsb(5) => 5 => 0101 => 010(1) => 0001 => 1
+	private int lsb(int i) {
+
+		// isolate the lowest one bit value
+		return i & -i;
+
+		// example1: i = 5 = 0101
+		// -i = 1011 (2's compliment)
+		// 0101 & 1011 = 0001 = 1
+
+		// example2: i = 8 = 1000
+		// -i = 1000 (2's compliment)
+		// 1000 & 1000 = 1000 = 8
+
+		// another alternate method is to use java's built
+		// in method return Integer.lowestOneBit(i);
+
+	}
+
+	// computes the prefix sum from [1, i], one based
+	public long prefixSum(int i) {
+
+		long sum = 0L;
+
+		while (i != 0) {
+
+			sum += tree[i];
+			i &= ~lsb(i); // pretty much i -= lsb(i)
+
+		}
+
+		return sum;
+
+	}
+
+	// returns the sum of the interval [i, j], one based
+	public long sum(int i, int j) {
+
+		if (j < i)
+			throw new IllegalArgumentException("j must be >= i");
+
+		return prefixSum(j) - prefixSum(i - 1);
+
+	}
+
+	// adds 'k' to index 'i', one based
+	public void add(int i, long k) {
+
+		while (i < tree.length) {
+			tree[i] += k;
+			i += lsb(i);
+		}
+
+	}
+
+	// set index i to be equal to k, one based
+	public void set(int i, long k) {
+		long value = sum(i, i);
+		add(i, k - value);
+	}
+
+	// returns a string of the fenwick tree
+	@Override
+	public String toString() {
+
+		String string = "";
+
+		for (int i = 0; i < tree.length; i++)
+			string += "[" + tree[i] + "]";
+
+		return string;
+
+	}
+
 }
--- FenwickTree_(BinaryIndexedTree)/src/fenwicktree_/binaryindexedtree/Main.java
+++ FenwickTree_(BinaryIndexedTree)/src/fenwicktree_/binaryindexedtree/Main.java
@@ -1,82 +1,29 @@
 package fenwicktree_.binaryindexedtree;
 
-import java.util.Scanner;
-
+/**
+ * @author sammyalsadek
+ *
+ */
 public class Main {
 
-    public static void main(String[] args) {
-        long [] array = {0,1,2,3,4,5,6,7,8,9};
-        FenwickTree ft = new FenwickTree(array);
-        Scanner scan = new Scanner(System.in);
-        String choice;
-        
-        do {
-            
-            System.out.println("--------------MENU--------------");
-            System.out.println("(0) Exit Program");
-            System.out.println("(1) Add Value to an Index");
-            System.out.println("(2) Set Value of an Index");
-            System.out.println("(3) Get PrefixSum of an Index");
-            System.out.println("(4) Get Sum Between Two Indices");
-            System.out.println("(5) Print Fenwick Tree");
-            System.out.println();
-            System.out.print("Select Choice (0-5): ");
-            choice = scan.next();
-            System.out.println();
+	public static void main(String[] args) {
 
-            switch (choice) {
-                case "0":
-                    break;
-                case "1":
-                    System.out.print("Insert index you would like to add a value to: ");
-                    int index = scan.nextInt();
-                    System.out.println();
-                    System.out.print("Insert value you would like to add to the index: ");
-                    long number = scan.nextLong();
-                    System.out.println();
-                    ft.add(index, number);
-                    System.out.println(number + " added to index " + index + "!");
-                    System.out.println();
-                    break;  
-                case "2":
-                    System.out.print("Insert index you would like to set the value of: ");
-                    int index1 = scan.nextInt();
-                    System.out.println();
-                    System.out.print("Insert value you would like to set the index to: ");
-                    long value = scan.nextLong();
-                    System.out.println();
-                    ft.set(index1, value);
-                    System.out.println("Index " + index1 + " set to " + value + "!");
-                    System.out.println();
-                    break;  
-                case "3":
-                    System.out.print("Insert index you would like to get the PrefixSum for: ");
-                    int index2 = scan.nextInt();
-                    System.out.println();
-                    System.out.println("PrefixSum of index " + index2 + ": " + ft.prefixSum(index2));
-                    System.out.println();
-                    break;      
-                case "4":
-                    System.out.print("Insert first index (1st index <= 2nd index): ");
-                    int i = scan.nextInt();
-                    System.out.println();
-                    System.out.print("Insert second index (1st index <= 2nd index): ");
-                    int j = scan.nextInt();
-                    System.out.println();
-                    System.out.println("Sum between of index " + i + " and index " + j + ": " + ft.sum(i, j));
-                    System.out.println();
-                    break;    
-                case "5":
-                    System.out.println(ft.toString());
-                    System.out.println();
-                    break;
-                default:
-                    System.out.println("Invalid input");
-                    System.out.println();
-                    break;
-            }
+		// test fenwick tree class
+		int size = 10;
+		FenwickTree tree = new FenwickTree(size);
 
-        } while (!"0".equals(choice));
-    }
-    
+		for (int i = 1; i <= size; i++)
+			tree.set(i, 1);
+
+		for (int i = 1; i <= size; i++)
+			tree.add(i, 1);
+
+		System.out.print("Tree -> ");
+		for (int i = 1; i <= size; i++)
+			System.out.print("[" + tree.prefixSum(i) + "]");
+
+		System.out.print("\nFenwickTree -> " + tree.toString());
+
+	}
+
 }
