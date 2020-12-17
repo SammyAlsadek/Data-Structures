705858bb9834504fa7991bcc3332177883fa9f3f
Sammy Alsadek <sammyalsadek@Sammys-Air.lan>
2020-12-17 02:52:25 -0800

    finalized binarySearchTree

--- BinarySearchTree/src/binarysearchtree/Main.java
+++ BinarySearchTree/src/binarysearchtree/Main.java
@@ -1,86 +1,40 @@
 package binarysearchtree;
 
-import java.util.Scanner;
-
+/**
+ * @author sammyalsadek
+ *
+ */
 public class Main {
 
-    public static void main(String[] args) {
-        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
-        Scanner scan = new Scanner(System.in);
-        String choice; // users input
-        
-        do {
+	public static void main(String[] args) {
 
-            System.out.println("--------------MENU--------------");
-            System.out.println("(0) Exit Program");
-            System.out.println("(1) Add Element");
-            System.out.println("(2) Remove Element");
-            System.out.println("(3) Size of tree");
-            System.out.println("(4) isEmpty");
-            System.out.println("(5) Contains");
-            System.out.println("(6) PreOrder Traversal");
-            System.out.println("(7) InOrder Traversal");
-            System.out.println("(8) PostOrder Traversal");
-            System.out.println();
-            System.out.print("Select Choice (0-8): ");
-            choice = scan.next();
-            System.out.println();
+		// test binary search tree class
+		BinarySearchTree<Integer> tree = new BinarySearchTree<Integer>();
 
-            switch (choice) {
-                case "0":
-                    break;
-                case "1":
-                    System.out.print("What value would you like to add to the tree: ");
-                    int number = scan.nextInt();
-                    System.out.println();
-                    System.out.println("Was the number (" + number + ") added: " + tree.add(number));
-                    System.out.println();
-                    break;  
-                case "2":
-                    System.out.print("What value would you like to remove from the tree: ");
-                    int number1 = scan.nextInt();
-                    System.out.println();
-                    System.out.println("Was the number (" + number1 + ") removed: " + tree.remove(number1));
-                    System.out.println();
-                    break;   
-                case "3":
-                    System.out.println("The size of the tree is: " + tree.size());
-                    System.out.println();
-                    break;
-                case "4":
-                    System.out.println("Is the tree empty: " + tree.isEmpty());
-                    System.out.println();
-                    break;
-                case "5":
-                    System.out.print("What value would you like to search for in the tree: ");
-                    int number2 = scan.nextInt();
-                    System.out.println();
-                    System.out.println("Does the list contain (" + number2 + "): " + tree.contains(number2));
-                    System.out.println();
-                    break;    
-                case "6":
-                    System.out.println("PreOrder Traversal: ");
-                    tree.preOrder();
-                    System.out.println();
-                    break;
-                case "7":
-                    System.out.println("InOrder Traversal: ");
-                    tree.inOrder();
-                    System.out.println();
-                    break;
-                case "8":
-                    System.out.println("PostOrder Traversal: ");
-                    tree.postOrder();
-                    System.out.println();
-                    break;    
-                default:
-                    System.out.println("Invalid input");
-                    System.out.println();
-                    break;
-            }
+		for (int i = 0; i <= 10; i++)
+			tree.add(i);
 
-        } while (!"0".equals(choice));
+		for (int i = 20; i > 10; i--)
+			tree.add(i);
 
-    }
-    
+		for (int i = 0; i <= 20; i++)
+			if (i % 2 == 0)
+				tree.remove(i);
+
+		for (int i = 0; i <= 20; i += 3)
+			System.out.println("Contains " + i + " -> " + tree.contains(i));
+
+		System.out.println("\nTree size -> " + tree.size());
+		System.out.println("\nTree empty -> " + tree.isEmpty());
+
+		System.out.println("\nTree:");
+		System.out.print("PreOrder -> ");
+		tree.preOrder();
+		System.out.print("\nInOrder -> ");
+		tree.inOrder();
+		System.out.print("\nPostOrder -> ");
+		tree.postOrder();
+
+	}
+
 }
