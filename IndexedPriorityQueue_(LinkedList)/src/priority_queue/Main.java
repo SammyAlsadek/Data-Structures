4d12d71a189299e1c1c892228a5dab48b56358a1
Sammy Alsadek <sammyalsadek@Sammys-Air.lan>
2020-12-19 22:13:12 -0800

    finalized indexed priority queue

--- IndexedPriorityQueue_(LinkedList)/src/priority_queue/Main.java
+++ IndexedPriorityQueue_(LinkedList)/src/priority_queue/Main.java
@@ -1,112 +1,30 @@
 package priority_queue;
 
-import java.util.Scanner;
-
+/**
+ * @author sammyalsadek
+ *
+ */
 public class Main {
 
-    public static void main(String args[]) {
-        Priority_Queue<Integer> PQueue = new Priority_Queue();
-        Scanner scan = new Scanner(System.in);
-        String choice;
+	public static void main(String args[]) {
 
-        do {
-            System.out.println("--------------MENU--------------");
-            System.out.println("(0) Exit Program");
-            System.out.println("(1) Insert Element");
-            System.out.println("(2) Remove Maximum Element");
-            System.out.println("(3) Increase Element's Priority");
-            System.out.println("(4) Decrease Element's Priority");
-            System.out.println("(5) Display Priority Queue");
-            System.out.println();
-            System.out.print("Select Choice (0-5): ");
-            choice = scan.next();
-            System.out.println();
+		// test indexed priority queue class
+		Priority_Queue<Integer> pq = new Priority_Queue<Integer>();
 
-            switch (choice) {
-                case "0":
-                    break;
-                case "1": 
-                    try {
-                        System.out.print("Enter New Elements Value: ");
-                        int element = scan.nextInt();
-                        System.out.print("Enter New Elements Priority: ");
-                        int priority = scan.nextInt();
-                        PQueue.insert(element, priority);
-                        System.out.println();
-                        System.out.println("New Element Has Been Insterted ");
-                        System.out.println();
-                    } catch (Exception e) {
-                        System.out.println();
-                        System.out.println(e + ": argument must be an integer");
-                        System.out.println();
-                        scan.next();
-                    }
-                    break;
-                case "2":
-                    if (PQueue.remove_maximum() != null) {
-                        System.out.println("Maximum Element has been removed from Queue");
-                    } else {
-                        System.out.println("Priority Queue is empty");
-                    }
-                    System.out.println();
-                    break;
-                case "3":
-                    try {
-                        System.out.print("Enter the Element you would like to increase the priority of: ");
-                        int increase = scan.nextInt();
-                        System.out.print("Enter the amount you would like to increase the priority: ");
-                        int priorityInc = scan.nextInt();
-                        try {
-                            PQueue.increase(increase, priorityInc);
-                            System.out.println();
-                            System.out.println("Priority has been increased ");
-                            System.out.println();
-                        } catch (Exception e) {
-                            System.out.println();
-                            System.out.println(e);
-                            System.out.println();
-                        }
-                    } catch (Exception e) {
-                        System.out.println();
-                        System.out.println(e + ": argument must be an integer");
-                        System.out.println();
-                        scan.next();
-                    }
-                    break;
-                case "4":
-                    try {
-                        System.out.print("Enter the Element you would like to decrease the priority of: ");
-                        int decrease = scan.nextInt();
-                        System.out.print("Enter the amount you would like to decrease the priority: ");
-                        int priorityDec = scan.nextInt();
-                        try {
-                            PQueue.decrease(decrease, priorityDec);
-                            System.out.println();
-                            System.out.println("Priority has been decreased ");
-                            System.out.println();
-                        } catch (Exception e) {
-                            System.out.println();
-                            System.out.println(e);
-                            System.out.println();
-                        }
-                    } catch (Exception e) {
-                        System.out.println();
-                        System.out.println(e + ": argument must be an integer");
-                        System.out.println();
-                        scan.next();
-                    }
-                    break;
-                case "5":
-                    PQueue.display();
-                    System.out.println();
-                    break;
-                default:
-                    System.out.println("Choice input by user is invalid...");
-                    System.out.println();
-                    break;
-            }
+		for (int i = 0; i < 10; i++)
+			pq.insert(i, i);
 
-        } while (!"0".equals(choice));
-    }
+		for (int i = 0; i < 10; i += 2)
+			pq.increase(i, i);
+
+		for (int i = 0; i < 10; i += 3)
+			pq.decrease(i, i);
+
+		for (int i = 0; i < 3; i++)
+			pq.remove_maximum();
+
+		pq.display();
+
+	}
 
 }
--- IndexedPriorityQueue_(LinkedList)/src/priority_queue/Priority_Queue.java
+++ IndexedPriorityQueue_(LinkedList)/src/priority_queue/Priority_Queue.java
@@ -1,220 +1,265 @@
 package priority_queue;
 
-import java.util.*;
+/**
+ * @author sammyalsadek
+ *
+ */
+public class Priority_Queue<T extends Object> {
 
-public class Priority_Queue<T> implements Priority_Queue_Interface<T> {
+	//////////////////////////////////////////////////////////////////////////////
 
-    //declare variables 
-    private PNode maxElement;
+	// inner Priority_Node class
+	public class PNode {
 
-//////////////////////////////////////////////////////////////////////////////////
-    //inner Priority_Node class
-    public class PNode<T> {
+		// declare variables
+		private T element;
+		private int priority;
+		private PNode nextNode;
 
-        //declare variables
-        private T element;
-        private int priority;
-        private PNode nextNode;
+		//////////////////////////////////////////////////////////////////////////
 
-        //////////////////////////////////////////////////////////////////////////
-        //constructors
-        public PNode(T e, int p, PNode next) {
-            this.element = e;
-            this.priority = p;
-            this.nextNode = next;
-        }
+		// constructors
+		public PNode(T e, int p, PNode next) {
+			this.element = e;
+			this.priority = p;
+			this.nextNode = next;
+		}
 
-        public PNode() {
-            this.element = null;
-            this.priority = -1;
-            this.nextNode = null;
-        }
+		public PNode() {
+			this.element = null;
+			this.priority = -1;
+			this.nextNode = null;
+		}
 
-        //////////////////////////////////////////////////////////////////////////
-        //setter funtions 
-        public void setElement(T e) {
-            this.element = e;
-        }
+		//////////////////////////////////////////////////////////////////////////
 
-        public void setPriority(int p) {
-            this.priority = p;
-        }
+		// setter functions
+		public void setElement(T e) {
+			this.element = e;
+		}
 
-        public void setNextNode(PNode n) {
-            this.nextNode = n;
-        }
+		public void setPriority(int p) {
+			this.priority = p;
+		}
 
-        //getter functions
-        public T getElement() {
-            return this.element;
-        }
+		public void setNextNode(PNode n) {
+			this.nextNode = n;
+		}
 
-        public int getPriority() {
-            return this.priority;
-        }
+		// getter functions
+		public T getElement() {
+			return this.element;
+		}
 
-        public PNode getNextNode() {
-            return this.nextNode;
-        }
+		public int getPriority() {
+			return this.priority;
+		}
 
-    }
+		public PNode getNextNode() {
+			return this.nextNode;
+		}
 
-//////////////////////////////////////////////////////////////////////////////////
-    //constructors
-    public Priority_Queue() {
-        this.maxElement = null;
-    }
+	}
 
-    public Priority_Queue(T element, int priority) {
-        this.maxElement = new PNode(element, priority, null);
-    }
+	//////////////////////////////////////////////////////////////////////////////
 
-//////////////////////////////////////////////////////////////////////////////////
-    //methods
-    //insert an element into the queue with a given priority (higher 
-    //priority value, the higher the priority is for the data element)
-    public void insert(T element, int priority) {
+	// declare variables
+	private PNode maxElement;
 
-        //create a new Priority Node
-        PNode newNode = new PNode(element, priority, null);
+	//////////////////////////////////////////////////////////////////////////////
 
-        //check to see if the Priority_Queue is empty
-        //if it is insert in the front of the list
-        if (this.maxElement == null) {
-            this.maxElement = newNode;
-        } else {
-            //place Node in coorect posistion within the Queue
-            placeNode(newNode);
-        }
+	// constructors
+	public Priority_Queue() {
+		this.maxElement = null;
+	}
 
-    }
+	public Priority_Queue(T element, int priority) {
+		this.maxElement = new PNode(element, priority, null);
+	}
 
-    //removes the highest priority element from the priority queue
-    public T remove_maximum() {
+	//////////////////////////////////////////////////////////////////////////////
 
-        T value = null;
-        
-        //check to see if the Queue is Empty
-        if (this.maxElement != null) {
-            value = (T) this.maxElement.getElement();
-            PNode oldMax = this.maxElement;
-            this.maxElement = this.maxElement.getNextNode();
-            oldMax.setNextNode(null);
-            oldMax = null;
-        }
+	// insert an element into the queue with a given priority (higher
+	// priority value, the higher the priority is for the data element)
+	public void insert(T element, int priority) {
 
-        return value;
+		// create a new Priority Node
+		PNode newNode = new PNode(element, priority, null);
 
-    }
-    
-    //places Node in proper position withing the queue
-    public void placeNode(PNode newNode) {
-        //check to see if the new Priority Node has 
-        //the greatest priority value
-        if (newNode.getPriority() > this.maxElement.priority) {
-            newNode.setNextNode(this.maxElement);
-            this.maxElement = newNode;
-        } //traverse through the list and find where the new
-        //Priority Node belongs (stop when reaching a node 
-        //with a smaller Priority)
-        else {
-            PNode prevNode = null;
-            PNode tempNode = this.maxElement;
-            while (newNode.getPriority() <= tempNode.getPriority() && tempNode.getNextNode() != null) {
-                prevNode = tempNode;
-                tempNode = tempNode.getNextNode();
-            }
-            //if our new Priority Nodes value is the minimum
-            //place it at the end of the Queue
-            if (newNode.getPriority() <= tempNode.getPriority() && tempNode.getNextNode() == null) {
-                tempNode.setNextNode(newNode);
-                newNode.setNextNode(null);
-            } else {
-                prevNode.setNextNode(newNode);
-                newNode.setNextNode(tempNode);
-            }
-        }
-    }
+		// check to see if the Priority_Queue is empty
+		// if it is insert in the front of the list
+		if (this.maxElement == null)
+			this.maxElement = newNode;
 
-    //search for element in the priority queue and decrease its priority
-    //to the new priority level (priority = priority – priority_delta)
-    public void decrease(T element, int priority_delta) {
-        
-        //check to see if priority_delta is a negative number
-        if (priority_delta < 0) throw new IllegalArgumentException("Cannot input a negative number as an argument");
-        //loop through the Queue to find the element
-        PNode prevNode = null;
-        PNode tempNode = this.maxElement;
-        while (tempNode != null && tempNode.getElement() != element) {
-            prevNode = tempNode;
-            tempNode = tempNode.getNextNode();
-        }
-        if (tempNode != null && tempNode.getElement() == element) {
-            tempNode.setPriority(tempNode.getPriority() - priority_delta);
-            if (tempNode.getNextNode() != null && tempNode.getPriority() <= tempNode.getNextNode().getPriority()) {
-                if (tempNode == this.maxElement) {
-                    remove_maximum();
-                } else {
-                    //remove the tempNode from its existing position within the list
-                    prevNode.setNextNode(tempNode.getNextNode());
-                }
-                //place Node in coorect posistion within the Queue
-                placeNode(tempNode);
-            }
-        } else {
-            throw new IllegalArgumentException("Element not found / Empty List");
-        }
-        
-    }
+		// place Node in correct position within the Queue
+		else
+			placeNode(newNode);
 
-    //search for element in the priority queue and increase its priority 
-    //to the new priority level (priority = priority + priority_delta)
-    public void increase(T element, int priority_delta) {
+	}
 
-        //check to see if priority_delta is a negative number
-        if (priority_delta < 0) throw new IllegalArgumentException("Cannot input a negative number as an argument");
-        //loop through the Queue to find the element
-        PNode prevNode = null;
-        PNode tempNode = this.maxElement;
-        while (tempNode != null && tempNode.getElement() != element) {
-            prevNode = tempNode;
-            tempNode = tempNode.getNextNode();
-        }
-        if (tempNode != null && tempNode.getElement() == element) {
-            tempNode.setPriority(tempNode.getPriority() + priority_delta);
-            if (prevNode != null && tempNode.getPriority() > prevNode.getPriority()) {
-                //remove the tempNode from its existing position within the list
-                prevNode.setNextNode(tempNode.getNextNode());
-                //place Node in coorect posistion within the Queue
-                placeNode(tempNode);
-            }
-        } else {
-            throw new IllegalArgumentException("Element not found / Empty List");
-        }
+	// removes the highest priority element from the priority queue
+	public T remove_maximum() {
 
-    }
+		T value = null;
 
-    //display the elements in the priority queue in order from highest 
-    //priority to lowest priority
-    public void display() {
+		// check to see if the Queue is Empty
+		if (this.maxElement != null) {
 
-        String string = "";
+			value = (T) this.maxElement.getElement();
+			PNode oldMax = this.maxElement;
+			this.maxElement = this.maxElement.getNextNode();
+			oldMax.setNextNode(null);
+			oldMax = null;
 
-        //check to see if the list is empty
-        if (this.maxElement == null) {
-            string += "[Empty Queue]";
-        } else {
-            string += "(HIGHEST PRIORITY) ";
-            PNode tempNode = this.maxElement;
-            while (tempNode != null) {
-                string += "[" + tempNode.getElement() + "]";
-                tempNode = tempNode.getNextNode();
-            }
-            string += " (LOWEST PRIORITY)";
-        }
+		}
 
-        System.out.println(string);
+		return value;
 
-    }
+	}
+
+	// places Node in proper position within the queue
+	private void placeNode(PNode newNode) {
+
+		// check to see if the new Priority Node has
+		// the greatest priority value
+		if (newNode.getPriority() > this.maxElement.priority) {
+			newNode.setNextNode(this.maxElement);
+			this.maxElement = newNode;
+		}
+
+		// traverse through the list and find where the new
+		// Priority Node belongs (stop when reaching a node
+		// with a smaller Priority)
+		else {
+
+			PNode prevNode = null;
+			PNode tempNode = this.maxElement;
+
+			while (newNode.getPriority() <= tempNode.getPriority() && tempNode.getNextNode() != null) {
+				prevNode = tempNode;
+				tempNode = tempNode.getNextNode();
+			}
+
+			// if our new Priority Nodes value is the minimum
+			// place it at the end of the Queue
+			if (newNode.getPriority() <= tempNode.getPriority() && tempNode.getNextNode() == null) {
+				tempNode.setNextNode(newNode);
+				newNode.setNextNode(null);
+			}
+
+			else {
+				prevNode.setNextNode(newNode);
+				newNode.setNextNode(tempNode);
+			}
+
+		}
+
+	}
+
+	// search for element in the priority queue and decrease its priority
+	// to the new priority level (priority = priority – priority_delta)
+	public void decrease(T element, int priority_delta) {
+
+		// check to see if priority_delta is a negative number
+		if (priority_delta < 0)
+			throw new IllegalArgumentException("Cannot input a negative number as an argument");
+
+		// loop through the Queue to find the element
+		PNode prevNode = null;
+		PNode tempNode = this.maxElement;
+		while (tempNode != null && tempNode.getElement() != element) {
+			prevNode = tempNode;
+			tempNode = tempNode.getNextNode();
+		}
+
+		if (tempNode != null && tempNode.getElement() == element) {
+
+			tempNode.setPriority(tempNode.getPriority() - priority_delta);
+
+			if (tempNode.getNextNode() != null && tempNode.getPriority() <= tempNode.getNextNode().getPriority()) {
+
+				if (tempNode == this.maxElement)
+					remove_maximum();
+
+				// remove the tempNode from its existing position within the list
+				else
+					prevNode.setNextNode(tempNode.getNextNode());
+
+				// place Node in correct position within the Queue
+				placeNode(tempNode);
+
+			}
+
+		}
+
+		else
+			throw new IllegalArgumentException("Element not found / Empty List");
+
+	}
+
+	// search for element in the priority queue and increase its priority
+	// to the new priority level (priority = priority + priority_delta)
+	public void increase(T element, int priority_delta) {
+
+		// check to see if priority_delta is a negative number
+		if (priority_delta < 0)
+			throw new IllegalArgumentException("Cannot input a negative number as an argument");
+
+		// loop through the Queue to find the element
+		PNode prevNode = null;
+		PNode tempNode = this.maxElement;
+		while (tempNode != null && tempNode.getElement() != element) {
+			prevNode = tempNode;
+			tempNode = tempNode.getNextNode();
+		}
+
+		if (tempNode != null && tempNode.getElement() == element) {
+
+			tempNode.setPriority(tempNode.getPriority() + priority_delta);
+
+			if (prevNode != null && tempNode.getPriority() > prevNode.getPriority()) {
+
+				// remove the tempNode from its existing position within the list
+				prevNode.setNextNode(tempNode.getNextNode());
+
+				// place Node in correct position within the Queue
+				placeNode(tempNode);
+
+			}
+
+		}
+
+		else
+			throw new IllegalArgumentException("Element not found / Empty List");
+
+	}
+
+	// display the elements in the priority queue in order from highest
+	// priority to lowest priority
+	public void display() {
+
+		String string = "";
+
+		// check to see if the list is empty
+		if (this.maxElement == null)
+			string += "[Empty Queue]";
+
+		else {
+
+			string += "(HIGHEST PRIORITY) ";
+			PNode tempNode = this.maxElement;
+
+			while (tempNode != null) {
+				string += "[" + tempNode.getElement() + "]";
+				tempNode = tempNode.getNextNode();
+			}
+
+			string += " (LOWEST PRIORITY)";
+
+		}
+
+		System.out.println(string);
+
+	}
 
 }
